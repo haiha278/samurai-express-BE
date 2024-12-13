da609a6235a27abce3b57a7a38be77bc9335c07c
LE HAI HA <haiha278@lgcns.com>
2024-12-13 16:11:25 +0700

    update pom file to upload imageto aws s3

--- src/main/java/lg/cns/restaurent_management_system/service/ProductService.java
+++ src/main/java/lg/cns/restaurent_management_system/service/ProductService.java
@@ -1,9 +1,13 @@
 package lg.cns.restaurent_management_system.service;
 
 import lg.cns.restaurent_management_system.dto.productDTO.GetAllProductByCategoryDTO;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ProductService {
     List<GetAllProductByCategoryDTO> getAllProductByCategory(String categoryName);
+
+    String uploadProductImage(MultipartFile multipartFile) throws IOException;
 }
--- src/main/java/lg/cns/restaurent_management_system/service/ProductServiceImpl.java
+++ src/main/java/lg/cns/restaurent_management_system/service/ProductServiceImpl.java
@@ -4,14 +4,24 @@ import lg.cns.restaurent_management_system.dto.productDTO.GetAllProductByCategor
 import lg.cns.restaurent_management_system.mapper.Mapper;
 import lg.cns.restaurent_management_system.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
     private ProductRepository productRepository;
+    private S3Client s3Client;
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
 
     @Autowired
     public ProductServiceImpl(ProductRepository productRepository) {
@@ -25,5 +35,16 @@ public class ProductServiceImpl implements ProductService{
             return productDTO;
         }).collect(Collectors.toList());
         return productList;
+    }
+
+    @Override
+    public String uploadProductImage(MultipartFile multipartFile) throws IOException {
+        String fileName = UUID.randomUUID() + "_" + multipartFile.getOriginalFilename();
+        s3Client.putObject(PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(multipartFile.getContentType())
+                .build(), RequestBody.fromBytes(multipartFile.getBytes()));
+        return "https://" + bucketName + ".s3.amazonaws.com/" + fileName;
     }
 }
